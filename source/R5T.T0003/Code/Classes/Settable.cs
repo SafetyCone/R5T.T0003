f0048b18eb9a3db1ece14da7211dfc55b7640b88
MinexAutomation <minex.automation@gmail.com>
2020-06-10 21:58:50 -0700

    Added Settable<T> class.

---
+++ source/R5T.T0003/Code/Classes/Settable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace R5T.T0003
+{
+    /// <summary>
+    /// A class that tracks whether its 
+    /// </summary>
+    public sealed class Settable<T>
+    {
+        public static implicit operator Settable<T>(T value) => new Settable<T>(value);
+
+
+        private T zValue;
+        public T Value
+        {
+            get
+            {
+                return this.zValue;
+            }
+            set
+            {
+                this.zValue = value;
+
+                this.IsSet = true;
+            }
+        }
+        public bool IsSet { get; private set; }
+
+
+        /// <summary>
+        /// Constructor with unset value.
+        /// </summary>
+        public Settable()
+        {
+            this.zValue = default(T);
+        }
+
+        /// <summary>
+        /// Allows setting the value at construction.
+        /// </summary>
+        public Settable(T value)
+        {
+            this.Value = value; // Will be set.
+        }
+
+        public override string ToString()
+        {
+            var representation = $"{this.Value} ({(this.IsSet ? "set" : "unset")})";
+            return representation;
+        }
+    }
+}
