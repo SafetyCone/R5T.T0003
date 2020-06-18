8e55a211615e50dc4f7ebf3be47742dcafe2b4f4
MinexAutomation <minex.automation@gmail.com>
2020-06-18 15:01:22 -0700

    Added CloneFrom() settable extension.

---
+++ source/R5T.T0003/Code/Extensions/SettableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace R5T.T0003
+{
+    public static class SettableExtensions
+    {
+        public static void CloneFrom<T>(this Settable<T> settable, Settable<T> other)
+        {
+            if(other.IsSet)
+            {
+                settable.Value = other.Value;
+            }
+            else
+            {
+                settable.Unset();
+            }
+        }
+    }
+}
